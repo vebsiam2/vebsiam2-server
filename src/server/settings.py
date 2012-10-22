0bff9053560ac5fe0354374357f8b24f317269b3
Eswar Vandanapu <eswar.vandanapu@vebsiam2.com>
2012-10-21 20:12:08 -0700

    Added gunicorn for production execution

--- src/server/settings.py
+++ src/server/settings.py
@@ -124,6 +124,11 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gunicorn',
+    # Modules added by us
+    'api',
+    'model',
+    'ui',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
