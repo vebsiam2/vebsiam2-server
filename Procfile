d835a3e9fa3206c45f97d1382b19129cdbcf1e68
Eswar Vandanapu <eswar.vandanapu@vebsiam2.com>
2012-11-03 23:32:30 -0700

    Fixed static file serving.

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: pythom src/manage.py collectstatic; python src/manage.py run_gunicorn -b 0.0.0.0:$PORT --workers=4
+web: python src/manage.py collectstatic --noinput; python src/manage.py run_gunicorn server.wsgi -b 0.0.0.0:$PORT --workers=4
--- src/server/urls.py
+++ src/server/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from api import user
+from server import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -7,6 +8,7 @@ from api import user
 
 urlpatterns = patterns('',
     url(r'^user/', include('api.user')),
+    url(r'^static/(.*)$', 'django.views.static.serve', {'document_root':settings.STATIC_ROOT}),
                        
     # Examples:
     # url(r'^$', 'server.views.home', name='home'),
