5a2c2e8535bd0598c316ea7bc73750ef15309b8a
Eswar Vandanapu <eswar.vandanapu@vebsiam2.com>
2012-11-03 23:48:14 -0700

    Changed the DB to postgress

--- src/server/settings.py
+++ src/server/settings.py
@@ -1,7 +1,5 @@
 # Django settings for server project.
-import os
-import pymongo
-import urlparse
+import os, pymongo, urlparse, dj_database_url
 
 TRUE_VALUES = ("true","t","1","yes")
 
@@ -25,14 +23,7 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(SITE_ROOT,'sqlite.db'),  # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
+    'default': dj_database_url.config(default='postgres://localhost/')
 }
 
 # Local time zone for this installation. Choices can be found here:
